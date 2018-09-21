30b2a7965d63823b90a49cedf24ae714e979fa17
Luke <lukemiller58@yahoo.com>
2018-09-21 10:59:52 -0500

    added main page

---
+++ app.js
@@ -0,0 +1,20 @@
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const staticMiddleWare = express.static('public');
+const views = require('./views');
+
+app.use(staticMiddleWare);
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  console.log('were in our homepage!');
+  res.send(views.main());
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
