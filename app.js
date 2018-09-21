15e9149b17534459fc4c2eed55e3ced48a1c3cae
sacreutz <sacreutz@gmail.com>
2018-09-21 11:56:58 -0500

    first

--- app.js
+++ app.js
@@ -3,18 +3,37 @@ const express = require('express');
 const app = express();
 const staticMiddleWare = express.static('public');
 const views = require('./views');
+const models = require('./models');
+console.log(models);
+
+models.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 console.log('hello sophie');
 app.use(staticMiddleWare);
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = 3000;
+
 
 app.get('/', (req, res) => {
   console.log('were in our homepage!');
   res.send(views.main());
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const PORT = 3000;
+const init = async () => {
+  await db.sync()
+
+
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+
+}
+
+init();
+
+
+
